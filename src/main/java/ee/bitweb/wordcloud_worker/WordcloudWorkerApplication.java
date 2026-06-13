578dca31ec17d84ea0c05eabce1aa1f1b9ffacf3
Kristo Erte <kristo.erte@gmail.com>
2026-06-13 16:37:21 +0300

    Create  project and add config files

---
+++ src/main/java/ee/bitweb/wordcloud_worker/WordcloudWorkerApplication.java
@@ -0,0 +1,13 @@
+package ee.bitweb.wordcloud_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcloudWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordcloudWorkerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ee/bitweb/wordcloud_worker/config/RabbitMQConfig.java
@@ -0,0 +1,25 @@
+package ee.bitweb.wordcloud_worker.config;
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter());
+        return factory;
+    }
+}
